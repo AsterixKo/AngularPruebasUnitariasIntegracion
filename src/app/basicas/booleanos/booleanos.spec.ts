e7928f315c94fadd2f70ce5792ed51db7d53d681
MSI\moneo <jvr.moneo@gmail.com>
2021-02-26 12:48:01 +0100

    Fin sección 22 - Pruebas Unitarias

---
+++ src/app/basicas/booleanos/booleanos.spec.ts
@@ -0,0 +1,14 @@
+import { usuarioIngresado } from "./booleanos";
+
+
+describe('Pruebas de Booleanos', () => {
+
+    it('Debe de retornar true', () => {
+
+        const res = usuarioIngresado();
+
+        expect(res).toBeTruthy();
+        // expect(res).not.toBeTruthy();
+        // expect(res).toBeFalsy();
+    });
+});
