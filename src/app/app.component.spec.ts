626264f29664874557a0bb2c6f2d168b83295d47
MSI\moneo <jvr.moneo@gmail.com>
2021-02-28 17:30:52 +0100

    Fin sección 25 - Pruebas de integración intermedias y avanzadas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { RouterTestingModule } from "@angular/router/testing";
+import { By } from "@angular/platform-browser";
+import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('AppComponent', () => {
+
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
@@ -22,10 +34,39 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pruebas');
   });
 
-  it('should render title', () => {
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('pruebas app is running!');
+  // });
+
+  it('Debe de tener un router-outlet', () => {
+
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pruebas app is running!');
+    const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(debugElement).not.toBeNull();
   });
+
+  // xit('Debe de tener un link a la página de médicos', () => {
+
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const elementos = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+  //   // console.log(elementos);
+
+  //   let existe = false;
+
+  //   for (const elem of elementos) {
+  //     if (elem.attributes['routerLink'] === '/medicos') {
+  //       existe = true;
+  //       break;
+  //     }
+  //   }
+
+  //   expect(existe).toBeTruthy();
+
+  // });
+
 });
